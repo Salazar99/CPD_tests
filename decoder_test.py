960339c28eb85810e05860c03e7b3d7168d8a520
SushiAndCode <simone.brunello@mail.polimi.it>
2026-02-23 00:53:51 +0100

    Adjust the problem with dataloader and dataset, add hyperparameter config setup at the beginning of the file

--- decoder_test.py
+++ decoder_test.py
@@ -4,21 +4,44 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from src.trace_container.trace_obj import Trace
 
+# General hyper param
 lr=1e-3
 latent_dim=16
 window_size=50
-batch_size=70
 epochs = 50
+
+# Hyper param encoder
+
+## CNN
+in_channels_first_layer = 1
+in_channels_second_layer = 16
+
+out_channels_first_layer = 16
+out_channels_second_layer = 32
+
+kernel_size_first_layer = 5
+kernel_size_second_layer = 5
+
+padding_first_layer = 2
+padding_second_layer = 2
+
+## LSTM
+input_size_lstm = 32
+hidden_size_lstm = 64
+
+## Linear layer
+in_feat_linea_layer = 64
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class SlidingWindowDataset(torch.utils.data.Dataset):
     def __init__(self, signal, window_size, stride):
         self.signal = torch.tensor(signal, dtype=torch.float32)
-        self.window_size = window_size
-        self.stride = stride
+        self.window_size = window_size # the dimension of the window
+        self.stride = stride # how much the windows moves at every step
 
     def __len__(self):
+        # In how many parts the signal is divided
         return (len(self.signal) - self.window_size) // self.stride + 1
 
     def __getitem__(self, idx):
@@ -31,17 +54,17 @@ class Encoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.cnn = nn.Sequential(
-            nn.Conv1d(1, 16, kernel_size=5, padding=2),
+            nn.Conv1d(in_channels_first_layer, out_channels_first_layer, kernel_size=kernel_size_first_layer, padding=padding_first_layer),
             nn.ReLU(),
-            nn.Conv1d(16, 32, kernel_size=5, padding=2),
+            nn.Conv1d(in_channels_second_layer, out_channels_second_layer, kernel_size=kernel_size_second_layer, padding=padding_second_layer),
             nn.ReLU()
         ).to(device)
         self.lstm = nn.LSTM(
-            input_size=32,
-            hidden_size=64,
+            input_size=input_size_lstm,
+            hidden_size=hidden_size_lstm,
             batch_first=True
         ).to(device)
-        self.fc = nn.Linear(64, latent_dim).to(device)
+        self.fc = nn.Linear(in_feat_linea_layer).to(device)
 
     def forward(self, x):
         # x: (B, 1, W)
@@ -97,14 +120,14 @@ criterion = nn.MSELoss()
 WT_trace = Trace("./tests/WT/WT_Trace_Date_repetition.csv")
 signal = torch.Tensor(WT_trace.get_column("rain")).to(device)
 
-dataset = SlidingWindowDataset(signal, window_size, stride=1)
-#loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+dataset = SlidingWindowDataset(signal, window_size=window_size, stride=window_size)
+loader = DataLoader(dataset, batch_size=window_size, shuffle=False)
 
 model.train()
 for epoch in range(epochs):
     total_loss = 0
     
-    for x in tqdm(dataset, desc=f"Epoch {epoch+1}/{epochs}", leave=False):
+    for x in tqdm(loader, desc=f"Epoch {epoch+1}/{epochs}", leave=False):
         x_hat, _ = model(x)
         loss = criterion(x_hat, x)
 
@@ -119,7 +142,7 @@ for epoch in range(epochs):
 model.eval()  
 
 with torch.no_grad():  
-    for x in DataLoader(dataset, batch_size=batch_size, shuffle=False):
+    for x in DataLoader(dataset, batch_size=window_size, shuffle=False):
         _, z = model(x)   
         
         latentVec.append(z.cpu())
